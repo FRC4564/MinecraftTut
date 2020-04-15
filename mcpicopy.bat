b447ce5edb2a0a9079483871772e244e940af95e
Steven Jacobs <stevejacobs@iname.com>
2020-04-15 17:18:15 -0400

    Verbose and pause

--- mcpicopy.bat
+++ mcpicopy.bat
@@ -1,3 +1,5 @@
 rem Copy mcpi folder to the Python 3.8 library
-copy mcpi C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python38-32\Lib
+mkdir %SYSTEMDRIVE%\Users\%USERNAME%\AppData\Local\Programs\Python\Python38-32\Lib\mcpi
+copy /v mcpi\*.* %SYSTEMDRIVE%\Users\%USERNAME%\AppData\Local\Programs\Python\Python38-32\Lib\mcpi\
+pause
 
